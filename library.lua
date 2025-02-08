3826ac77929edb718e813e167f0ce57fa002ce46
River <100126035+eLriver@users.noreply.github.com>
2025-02-08 21:48:42 +0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -45,6 +45,7 @@ function Library:CreateWindow(Config, Parent)
 	local Folder = game:GetObjects("rbxassetid://98051425121243")[1]
 	local Screen = Folder.Bracket:Clone()
 	local MobileToggle = Screen.MobileToggle
+	Mobile.Position = UDim2.new(1,50,1,15)
 	local Main = Screen.Main
 	local Holder = Main.Holder
 	local Topbar = Main.Topbar
